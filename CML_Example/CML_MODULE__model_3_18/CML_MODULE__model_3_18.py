a3b2f87602cf0de87d2c113426228b7be76e2491
Zahra Zad <zad@bu.edu>
2022-11-12 15:50:44 -0500

    Add files via upload

---
+++ CML_Example/CML_MODULE__model_3_18/CML_MODULE__model_3_18.py
@@ -0,0 +1,75 @@
+def model_3_18():
+    
+    import numpy as np
+    import pandas as pd
+    import csv
+    from sklearn import preprocessing
+    from sklearn.metrics import roc_curve, auc, f1_score, precision_score, recall_score, precision_recall_curve, plot_precision_recall_curve 
+    from sklearn.ensemble import RandomForestClassifier                                             
+    from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, KFold, StratifiedKFold 
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.svm import LinearSVC
+    from sklearn.neural_network import MLPClassifier 
+    import lightgbm as lgb
+    from sklearn.feature_selection import RFE,RFECV
+    import scipy.io
+    from scipy import stats
+    import matplotlib.pyplot as plt
+    #import seaborn as sns
+    import sklearn
+    import math
+    import pickle
+    import warnings
+    warnings.filterwarnings("ignore")
+    sklearn.__version__
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+
+
+    ## input
+    Mol_3m_IM = None
+    while Mol_3m_IM is None:
+        try:
+            Mol_3m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 3 months after start of Imatinib: "))
+        except ValueError:
+            print('The input value for BCR-ABL1 ratio (percantage) at 3 months is invalid.')
+            Mol_3m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 3 months after start of Imatinib: "))
+
+    WBC_count_at_diagnosis = None
+    while WBC_count_at_diagnosis is None:
+        try:
+            WBC_count_at_diagnosis = float(input("Enter White Blood Cell count at diagnosis: "))
+        except ValueError:
+            print('The input value for White Blood Cell count at diagnosis is invalid.')
+            WBC_count_at_diagnosis = float(input("Enter White Blood Cell count at diagnosis: "))    
+
+
+    ## dataframe
+    variable_dict = {
+
+        'Mol_3m_IM' : Mol_3m_IM,
+        'WHITE_BLOOD_CELL_COUNT_AT_DIAGNOSIS': (WBC_count_at_diagnosis - 119631.358025) / 95581.016130, #standardization
+    }
+
+    df = pd.DataFrame(columns=list(list(variable_dict.keys())))
+    df = df.append(variable_dict, ignore_index=True)
+
+    cols_Mol = ['Mol_3m_IM']
+    df['Mol_median'] = df[cols_Mol].median(axis=1)
+    df['Mol_median'] = (df['Mol_median'] - 14.960259) / 22.275554 #standardization         
+
+
+    ## loading models
+    my_seeds_end = 2040
+    prob = []
+    pred = []
+    my_seeds=range(2020, my_seeds_end) # the random_state that controls the shuffling applied to the data before applying the split
+    for seed in my_seeds:
+        filename = f'clf_{seed}'
+        clf = f'clf_{seed}'
+        clf = pickle.load(open(filename, 'rb'))
+        pred.append(clf.predict(df[['Mol_median', 'WHITE_BLOOD_CELL_COUNT_AT_DIAGNOSIS']].values))
+        prob.append(clf.predict_proba(df[['Mol_median', 'WHITE_BLOOD_CELL_COUNT_AT_DIAGNOSIS']].values)[:,1])
+
+    mean = sum(prob) / len(prob)
+    print('The probability of Molecular Response achievement at 18 months after start of Imatinib tretment, using patient information up to 3 months after start of Imatinib, is: ' + str(round(mean[0],2)))
---
+++ CML_Example/CML_MODULE__model_longDMR_12/CML_MODULE__model_12_long.py
@@ -0,0 +1,102 @@
+def model_12_long():
+    
+    import numpy as np
+    import pandas as pd
+    import csv
+    from sklearn import preprocessing
+    from sklearn.metrics import roc_curve, auc, f1_score, precision_score, recall_score, precision_recall_curve, plot_precision_recall_curve 
+    from sklearn.ensemble import RandomForestClassifier                                             
+    from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, KFold, StratifiedKFold 
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.svm import LinearSVC
+    from sklearn.neural_network import MLPClassifier 
+    import lightgbm as lgb
+    from sklearn.feature_selection import RFE,RFECV
+    import scipy.io
+    from scipy import stats
+    import matplotlib.pyplot as plt
+    #import seaborn as sns
+    import sklearn
+    import math
+    import pickle
+    import warnings
+    warnings.filterwarnings("ignore")
+    sklearn.__version__
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+
+
+    ## input
+    Mol_3m_IM = None
+    while Mol_3m_IM is None:
+        try:
+            Mol_3m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 3 months after start of Imatinib: "))
+        except ValueError:
+            print('The input value for BCR-ABL1 ratio (percantage) at 3 months is invalid.')
+            Mol_3m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 3 months after start of Imatinib: "))
+
+    Mol_6m_IM = None
+    while Mol_6m_IM is None:
+        try:
+            Mol_6m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 6 months after start of Imatinib: "))
+        except ValueError:
+            print('The input value for BCR-ABL1 ratio (percantage) at 6 months is invalid.')
+            Mol_6m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 6 months after start of Imatinib: "))
+
+    Mol_9m_IM = None
+    while Mol_9m_IM is None:
+        try:
+            Mol_9m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 9 months after start of Imatinib: "))
+        except ValueError:
+            print('The input value for BCR-ABL1 ratio (percantage) at 9 months is invalid.')
+            Mol_9m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 9 months after start of Imatinib: "))
+
+    Mol_12m_IM = None
+    while Mol_12m_IM is None:
+        try:
+            Mol_12m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 12 months after start of Imatinib: "))
+        except ValueError:
+            print('The input value for BCR-ABL1 ratio (percantage) at 12 months is invalid.')
+            Mol_12m_IM = float(input("Enter BCR-ABL1 ratio (percantage) at 12 months after start of Imatinib: "))
+
+    WBC_count_at_diagnosis = None
+    while WBC_count_at_diagnosis is None:
+        try:
+            WBC_count_at_diagnosis = float(input("Enter White Blood Cell count at diagnosis: "))
+        except ValueError:
+            print('The input value for White Blood Cell count at diagnosis is invalid.')
+            WBC_count_at_diagnosis = float(input("Enter White Blood Cell count at diagnosis: "))    
+
+
+    ## dataframe
+    variable_dict = {
+
+        'Mol_3m_IM' : Mol_3m_IM,
+        'Mol_6m_IM': Mol_6m_IM,
+        'Mol_9m_IM': Mol_9m_IM,
+        'Mol_12m_IM': Mol_12m_IM,
+        'WHITE_BLOOD_CELL_COUNT_AT_DIAGNOSIS': (WBC_count_at_diagnosis - 115829.052632) / 93768.243877, #standardization
+    }
+
+    df = pd.DataFrame(columns=list(list(variable_dict.keys())))
+    df = df.append(variable_dict, ignore_index=True)
+
+    cols_Mol = ['Mol_3m_IM','Mol_6m_IM','Mol_9m_IM','Mol_12m_IM']
+    df['Mol_median'] = df[cols_Mol].median(axis=1)
+    df['Mol_median'] = (df['Mol_median'] - 4.473496) / 10.153238 #standardization
+
+
+    ## loading models
+    my_seeds_end = 2040
+    prob = []
+    pred = []
+    my_seeds=range(2020, my_seeds_end) # the random_state that controls the shuffling applied to the data before applying the split
+    for seed in my_seeds:
+        filename = f'clf_{seed}'
+        clf = f'clf_{seed}'
+        clf = pickle.load(open(filename, 'rb'))
+        pred.append(clf.predict(df[['Mol_median', 'WHITE_BLOOD_CELL_COUNT_AT_DIAGNOSIS']].values))
+        prob.append(clf.predict_proba(df[['Mol_median', 'WHITE_BLOOD_CELL_COUNT_AT_DIAGNOSIS']].values)[:,1])
+
+    mean = sum(prob) / len(prob)
+    print('The probability of Deep Molecular Response achievement, using patient information up to 12 months after start of Imatinib, is: ' + str(round(mean[0],2)))
